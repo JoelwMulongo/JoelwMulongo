ba5321e1159ea7d3bc2ce5e0cbc9482066144459
JoelwMulongo <joelmulongo97@gmail.com>
2024-06-12 12:33:04 +0300

    update

--- updater.bat
+++ updater.bat
@@ -1,9 +1,28 @@
 @echo off
-
+REM Initialize the count variable to 0
+setlocal EnableDelayedExpansion
 set /A count=0
-FOR /F %%x in (updater.bat) DO set /A count += 1
-set /A count -= 8 
-echo ::%count% >> updater.bat 
+
+REM Loop through each line in updater.bat and increment the count
+for /F "delims=" %%x in (updater.bat) do (
+    set /A count += 1
+)
+
+REM Subtract 8 from the count
+set /A count -= 8
+
+REM Append the count as a comment to updater.bat
+echo ::!count! >> updater.bat
+
+REM Stage the changes in updater.bat
 git add updater.bat
-git commit -m "%date%"
+
+REM Get the current date in YYYY-MM-DD format
+for /F "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
+set commitMsg=!datetime:~0,4!-!datetime:~4,2!-!datetime:~6,2!
+
+REM Commit the changes with the formatted date as the message
+git commit -m "!commitMsg!"
+
+REM Push the changes to the remote repository
 git push
